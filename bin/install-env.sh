3704e0bbd5e2890aa08d462203dd60d3ae647faa
Pau Carré Cardona <pau.carre@gmail.com>
2023-07-16 17:30:02 +0200

    cleanup

--- bin/install-env.sh
+++ bin/install-env.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+pip uninstall poetry
 sudo apt install graphviz
-pip install poetry==1.5.1
-export PATH=$PATH:$HOME/.local/bin/
-poetry --directory=dev-gpu update
-poetry --directory=dev-gpu install
-poetry --directory=dev-gpu shell
+sudo apt-get install libreadline* libreadline*-dev libbz2-dev libssl-dev liblzma-dev libsqlite3-dev
+curl https://pyenv.run | bash
+echo 'export PATH=$PATH:$HOME/.local/bin/' >> ~/.bashrc
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+pyenv install 3.10.12
+eval "$(pyenv init -)"
+pyenv global 3.10.12
+python -m pip install poetry==1.5.1
+echo 'export PATH="$PYENV_ROOT/versions/3.10.12/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+python -m pip install keyring
+python -m keyring --disable
+poetry install
+poetry shell
--- linguamechanica/dql/dueling_ddqn_agent.py
+++ linguamechanica/dql/dueling_ddqn_agent.py
@@ -96,35 +96,23 @@ class DuelingDDQNAgent(object):
     def learn(self):
         if self.memory.mem_cntr < self.batch_size:
             return
-
         self.q_eval.optimizer.zero_grad()
-
         self.replace_target_network()
-
         states, actions, rewards, states_, dones = self.sample_memory()
         indices = np.arange(self.batch_size)
-
         V_s, A_s = self.q_eval.forward(states)
         V_s_, A_s_ = self.q_next.forward(states_)
-
         V_s_eval, A_s_eval = self.q_eval.forward(states_)
-
         q_pred = T.add(V_s, (A_s - A_s.mean(dim=1, keepdim=True)))[indices, actions]
-
         q_next = T.add(V_s_, (A_s_ - A_s_.mean(dim=1, keepdim=True)))
-
         q_eval = T.add(V_s_eval, (A_s_eval - A_s_eval.mean(dim=1, keepdim=True)))
-
         max_actions = T.argmax(q_eval, dim=1)
         q_next[dones] = 0.0
-
         q_target = rewards + self.gamma * q_next[indices, max_actions]
-
         loss = self.q_eval.loss(q_target, q_pred).to(self.q_eval.device)
         loss.backward()
         self.q_eval.optimizer.step()
         self.learn_step_counter += 1
-
         self.decrement_epsilon()
 
     def save_models(self):
